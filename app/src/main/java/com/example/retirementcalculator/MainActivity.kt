ad9fb1e56255f3d5d3524168259f977278b4f61c
Yevhen Khomiak <champdev1986@gmail.com>
2024-06-03 16:12:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/retirementcalculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.retirementcalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        AppCenter.start(
+            application, "79a51f36-9f17-4d44-a3cc-2d01b99712a8",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
